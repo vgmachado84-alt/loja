67b0d0faa770e15e6f72d9ea33a9908b9618d2d3
vgmachado84-alt <vgmachado84@gmail.com>
2026-04-18 22:47:04 +0000

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'centerpet-v1';
+const urlsToCache = [
+  '/loja/',
+  '/loja/index.html',
+  '/loja/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
